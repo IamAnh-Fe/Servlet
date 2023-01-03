14eeac004b4cda5e465ba0a72e5dea5bcae4c036
Pham Tien Anh <phamtienanhdev@gmail.com>
2023-01-03 23:36:28 +0700

    Service

---
+++ jsp-servlet-jdbc/src/main/java/com/laptrinhjavaweb/dao/ICategoryDAO.java
@@ -0,0 +1,8 @@
+package com.laptrinhjavaweb.dao;
+import java.util.List;
+import com.laptrinhjavaweb.model.CategoryModel;
+public interface ICategoryDAO extends GenericDAO<CategoryModel> {
+List<CategoryModel> findAll();
+CategoryModel findOne(long id);
+CategoryModel findOneByCode(string code);
+}
---
+++ jsp-servlet-jdbc/src/main/java/com/laptrinhjavaweb/dao/impl/CategoryDAO.java
@@ -0,0 +1,11 @@
+package com.laptrinhjavaweb.dao.impl;
+import java.sql.Connection;
+import java.util.List;
+import com.laptrinhjavaweb.dao.ICategoryDAO;
+import com.laptrinhjavaweb.model.CategoryModel;
+public class CategoryDAO implements ICategoryDAO {
+public Connection getConnection() {
+	
+}
+public List<>
+}
